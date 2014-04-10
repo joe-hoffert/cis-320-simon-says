1d77d8e29c94426ef1678873e6249c11b2e0da75
jeremydavis519@gmail.com <jeremydavis519@gmail.com@b1b370eb-38bb-dd47-555a-f25ee9e3f033>
2014-04-10 16:58:22 +0000

    US4:6 US4:11 Added isHighScore and made gameOver redirect the user to either new_high_score.html or highScore.html. Added createNewButtonSequence to randomize the button sequence every time it is called.

--- trunk/src/game_helpers.js
+++ trunk/src/game_helpers.js
@@ -20,11 +20,24 @@
  limitations under the License.
  *******************************************/
 
+/* Returns true if the given score would be placed on the high-score table and false otherwise. */
+function isHighScore(score) {
+    "use strict";
+    
+    var lowestScore = getHighScore(MAX_NUM_HIGHSCORES);
+    
+    return lowestScore === "" || lowestScore < score;
+}
+
 /* Called to end the game when the player runs out of lives. */
 function gameOver() {
     "use strict";
     
-    goBack();
+    if (isHighScore(currentScore)) {
+        window.href = "new_high_score.html";
+    } else {
+        window.href = "highScore.html";
+    }
 }
 
 /* Returns true if, according to the rules, a click of the given button is correct at this point in the sequence, and false otherwise. */
@@ -39,6 +52,18 @@ function addButtonToSequence() {
     "use strict";
     
     flashingButtonSequence[flashingButtonSequence.length] = Math.floor(Math.random() * NUM_BUTTONS);
+}
+
+/* Creates a new button sequence of the current length. */
+function createNewButtonSequence() {
+    "use strict";
+    
+    var i;
+    
+    flashingButtonSequence = [];
+    for (i = 0; i < sequenceLength; i += 1) {
+        addButtonToSequence();
+    }
 }
 
 /* Returns the "id" attribute of a button given its number. */
